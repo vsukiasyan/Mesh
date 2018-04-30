fdd7ec4193d8622843162a00064d0d810cd44b27
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-30 14:34:16 -0700

    Initial Commit

---
+++ Mesh/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mesh
+//
+//  Created by Vic Sukiasyan on 4/30/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
