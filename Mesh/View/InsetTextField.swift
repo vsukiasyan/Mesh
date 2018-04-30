4251dae1ce1d974a4cbc99a93203030a0e2b20e8
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-30 16:34:03 -0700

    Added the assets and started on the AuthVC and LoginVC.

---
+++ Mesh/View/InsetTextField.swift
@@ -0,0 +1,40 @@
+//
+//  InsetTextField.swift
+//  Mesh
+//
+//  Created by Vic Sukiasyan on 4/30/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    private var textRectOffset: CGFloat = 20
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    override func awakeFromNib() {
+        setupView()
+        super.awakeFromNib()
+        
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    func setupView() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        self.attributedPlaceholder = placeholder
+    }
+    
+}
---
+++ Mesh/View/ShadowView.swift
@@ -0,0 +1,23 @@
+//
+//  ShadowView.swift
+//  Mesh
+//
+//  Created by Vic Sukiasyan on 4/30/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = UIColor.black.cgColor
+        super.awakeFromNib()
+        
+    }
+
+
+
+}
