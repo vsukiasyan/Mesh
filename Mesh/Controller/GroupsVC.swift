5dfc08a9fde25b7afb0ae0039fe56593842666ff
vsukiasyan <vsukiasyan1@hotmail.com>
2018-05-02 20:06:56 -0700

    Group functionality is fully online.

--- Mesh/Controller/GroupsVC.swift
+++ Mesh/Controller/GroupsVC.swift
@@ -12,18 +12,26 @@ class GroupsVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var groupsArray = [Group]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.reloadData()
         
     }
 
-    
-    
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.tableView.reloadData()
+            }
+        }
+    }
 }
 
 extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
@@ -32,12 +40,14 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
-        cell.configureCell(title: "nerd herd", description: "The nerdies muthafuckas on the planet", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        
+        cell.configureCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         return cell
     }
 }
